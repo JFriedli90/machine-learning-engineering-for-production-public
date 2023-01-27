473b445bce8b69ac6c1af4684b1d1e46c34289ae
Friedli <JFriedli@htamericas.com>
2023-01-27 14:43:25 -0600

    Testing the CI/CD pipeline

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -4,7 +4,7 @@ from typing import List
 from fastapi import FastAPI
 from pydantic import BaseModel, conlist
 
-
+#THIS IS A HECKUVA GITHUB ACTION
 
 app = FastAPI(title="Predicting Wine Class with batching")
 
